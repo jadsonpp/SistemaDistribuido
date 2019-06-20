15d546311ecd9c1c03cd19fe1bf793495c988f48
jadsonpp <jadson_pereira5@hotmail.com>
2019-06-20 08:52:03 -0300

    att cliente para mandar msg continuamente

--- src/cliente.py
+++ src/cliente.py
@@ -1,29 +1,40 @@
 import pika
 import sys
+import time
 from random import uniform
 # Gera dados aleatoriamente.
 def geraTemperatura():
-    return uniform(10,40)
+    return str(uniform(10,40))
 #Gera a humidade aleatoriamente.
 def geraHumidade():
-    return uniform(40,90)
+    return str(uniform(40,90))
 #Porcentagem.
 def geraPressao():
-    return uniform(0,1)
+    return str(uniform(0,1))
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+def mandaMSG(msg:str):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+    message = ' '.join(sys.argv[1:]) or msg
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        ))
+    print(" [x] Sent %r" % message)
+    connection.close()
 
-channel.queue_declare(queue='task_queue', durable=True)
-
-message = ' '.join(sys.argv[1:]) or str(geraTemperatura())
-channel.basic_publish(
-    exchange='',
-    routing_key='task_queue',
-    body=message,
-    properties=pika.BasicProperties(
-        delivery_mode=2,  # make message persistent
-    ))
-print(" [x] Sent %r" % message)
-connection.close()
+while(True):
+    msg = geraTemperatura()
+    mandaMSG(msg)
+    time.sleep(10)
+    msg = geraHumidade()
+    mandaMSG(msg)
+    time.sleep(10)
+    msg = geraPressao()
+    mandaMSG(msg)
+    time.sleep(10)
